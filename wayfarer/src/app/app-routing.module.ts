df130229a13a27c4e7a81b16dbbe182b5c07ca07
ashleyshakir <ashleymshakir@gmail.com>
2023-10-14 17:11:25 -0400

    home page route created with city info

--- wayfarer/src/app/app-routing.module.ts
+++ wayfarer/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroComponent } from './landing-page/hero/hero.component';
 import { CityDetailsComponent } from './city-details/city-details.component';
+import { CitiesComponent } from './home-page/cities/cities.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HeroComponent
+  },
+  {
+    path: 'home', 
+    component: CitiesComponent
   },
   {
     path: 'home/:cityId', 
