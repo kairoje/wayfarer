a0cfe78d6fda967da1af90998efb1ac042e45f79
Ricardo Maya <rickmaya@gmail.com>
2023-10-15 19:39:00 -0500

    modify search component to set a debounce time and utilize the distinctUntilChanged method

--- wayfarer/src/app/landing-page/header/search/search.component.ts
+++ wayfarer/src/app/landing-page/header/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CitiesService } from '../../../home-page/cities/cities.service';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -8,17 +10,26 @@ import { CitiesService } from '../../../home-page/cities/cities.service';
 })
 export class SearchComponent {
   city: string = '';
+  searchSubject: Subject<string> = new Subject();
 
   constructor(private citiesService: CitiesService) { }
 
   findCity(cityName: string) {
     console.log('finding city', cityName);
-    this.citiesService.getCityByName(cityName)
-    .then(city => {
-      console.log('Found city:', city);
-    })
-    .catch(error => {
-      console.error('Error finding city:', error);
+    this.searchSubject.next(cityName);
+  }
+
+  ngOnInit(): void {
+    this.searchSubject
+    .pipe(debounceTime(1000), distinctUntilChanged())
+    .subscribe(cityName => {
+      this.citiesService.getCityByName(cityName)
+      .then(city => {
+        console.log('Found city:', city);
+      })
+      .catch(error => {
+        console.error('Error finding city:', error);
+      })
     })
   }
 }
